2a766fe4a8a4941dc1d83257f11223f7d317c502
Chase Vaughan <git@cvaugh.dev>
2022-10-22 15:12:33 -0400

    Add PatientStatus.toString()

--- src/ver1/PatientStatus.java
+++ src/ver1/PatientStatus.java
@@ -1,5 +1,19 @@
 package ver1;
 
 public enum PatientStatus {
-    WAITING, READY, ON_HOLD, IN_PROGRESS, CHECKED_OUT;
+    WAITING("Waiting"),
+    READY("Ready"),
+    ON_HOLD("On Hold"),
+    IN_PROGRESS("In Progress"),
+    CHECKED_OUT("Checked Out");
+
+    private String toString;
+
+    PatientStatus(String toString) {
+        this.toString = toString;
+    }
+
+    public String toString() {
+        return toString;
+    }
 }
