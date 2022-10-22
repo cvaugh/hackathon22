4a4e08f1fb3f21c30f3c30467e0bc5cc63090fa8
Lucas Keasbey <lkeasbey@gmail.com>
2022-10-22 12:03:38 -0400

    Made Patients sortable by status, then id

--- src/ver1/HospitalTest.java
+++ src/ver1/HospitalTest.java
@@ -3,11 +3,16 @@ package ver1;
 public class HospitalTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		test1();
 
 	}
 	
-	public static Hospital makeHospital() {
+	public void test1() {
+		Hospital h = makeHospital();
+		System.out.println(h);
+	}
+	
+	public Hospital makeHospital() {
 		Hospital h = new Hospital();
 		h.addRoom(1);
 		h.addRoom(2);
