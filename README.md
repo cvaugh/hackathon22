3d134a56cea53bb6ad1f8f70eaa145f9c30b571c
Chase Vaughan <git@cvaugh.dev>
2022-10-22 09:27:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Azalea Health Hackathon 2022
+
+Lucas Keasbey, Jasmine Merritt, Chase Vaughan
---
+++ src/hackathon/v1/Main.java
@@ -0,0 +1,7 @@
+package hackathon.v1;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
